e28b26bcf75fcb7fa34da3ce944a35352c8540ec
TheoMKgosi <theo.kgosiemang12@gmail.com>
2025-11-10 22:36:32 +0200

    Fix: site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/thinktech-botswana/',
+  site: 'https://theomkgosi.github.io/',
+  base: '/thinktech-botswana',
   vite: {
     plugins: [tailwindcss()]
   }
