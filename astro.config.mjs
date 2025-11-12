d31b592c6e03caabfedf08dd5f33c3f6c93c12e1
TheoMKgosi <theo.kgosiemang12@gmail.com>
2025-11-12 20:57:50 +0200

    Fix: Thinktech Botswana and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://theomkgosi.github.io/',
   base: '/thinktech-botswana',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [icon()]
 });
